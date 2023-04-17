45e0c0802283942b67b591a037eec189e9caff75
KAVIN S <104634566+KAVIN01-S@users.noreply.github.com>
2023-04-17 19:00:20 +0530

    Update inheritance.cs

--- OOPS concepts/inheritance.cs
+++ OOPS concepts/inheritance.cs
@@ -1,23 +1,29 @@
 using System; 
 
-namespace Emp{
+namespace Employee{
    public class Employee  
     {  
+        //surName with default value
         public string surName  = "Saravanan";
     }
-   class Programmer : Employee  
-   { 
-        private string name;
-        public string Name
-        { 
-            get{
-                return name;
-            } 
+   class Employee_details : Employee  
+   {
+        
+        private string firstName;
+        public string FirstName
+        //getter setter for firstName
+        {
+            get
+            {
+                return firstName;
+            }
             set{
-                name = value;
+                firstName = value;
             }
         }
-        public void Display(){
+        public void Display()
+        {
+            //Displaying the First Name with Last Name
             Console.WriteLine("Name : "+name +" "+surName) ;
         }
    }
@@ -25,8 +31,11 @@ namespace Emp{
    {  
        public static void Main(string[] args)  
         {  
-            Programmer p1 = new Programmer();
-            p1.Name= "Kavin";  
+            //Creating object for Employee_details class
+            Employee_details emp = new Employee_details();
+            //setting value for name variable
+            p1.FirstName= "Kavin";
+            //Calling display method in Employee_details class
             p1.Display();  
         }  
     }
