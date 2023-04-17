c2c5455fe08efb80c79b5bbc684984befd142dc3
KAVIN S <104634566+KAVIN01-S@users.noreply.github.com>
2023-04-17 22:56:06 +0530

    Update Encapsulation_1.cs

--- OOPS concepts/Encapsulation_1.cs
+++ OOPS concepts/Encapsulation_1.cs
@@ -4,12 +4,14 @@ namespace RectangleApplication {
     //Rectangle class
    class Rectangle {
       //member variables
+      //internal types or members are accessible only within files in the same assembly.
       internal double length;
       internal double width;
-      
+      //Method to return Area of Rectangle
       double GetArea() {
          return length * width;
       }
+      
       public void Display() {
          Console.WriteLine("Length: {0}", length);
          Console.WriteLine("Width: {0}", width);
@@ -17,15 +19,17 @@ namespace RectangleApplication {
       }
    }
    //end class Rectangle
-   
-
+    
    class Program{
-      static void Main(string[] args) {
+      public static void Main(string[] args) 
+      {
+          //Creating object for Rectangle class
          Rectangle r = new Rectangle();
          Console.WriteLine("Enter the length:");
          r.length = Convert.ToDouble(Console.ReadLine());
          Console.WriteLine("Enter the Width:");
          r.width = Convert.ToDouble(Console.ReadLine());
+         //Display method to display the area of rectangle
          r.Display();
       }
    }
