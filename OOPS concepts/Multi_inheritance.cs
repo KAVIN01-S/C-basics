2cd41cff7a6f8893d7c4ac06d361f83645acfa19
KAVIN S <104634566+KAVIN01-S@users.noreply.github.com>
2023-04-17 19:15:28 +0530

    Update Multi_inheritance.cs

--- OOPS concepts/Multi_inheritance.cs
+++ OOPS concepts/Multi_inheritance.cs
@@ -1,15 +1,18 @@
-using System; 
+using System;
 
-//MULTIPLE inheritance
+//HIERARCHICAL inheritance
 
-namespace Emp{
-   class Employee  
+namespace EmpLoyeeDetails
+{
+   //Employee class which stores surName
+   public class Employee  
     {  
+        //surName with default value
         public string surName  = "Saravanan";
     }
-    //Programmer1 is inherited from Employee
-    class Programmer1 : Employee  
-   { 
+    //Emp_1 is inherited from Employee
+    class Employee_1 : EmpLoyeeDetails
+    { 
         private string name;
         public string Name
         { 
@@ -24,8 +27,8 @@ namespace Emp{
             Console.WriteLine("Name : "+name +" "+surName) ;
         }
    }
-   //Programmer2 is inherited from Employee
-   class Programmer2: Employee
+   //Emp_2 is inherited from Employee
+   class Employee_2: EmpLoyeeDetails
    {
         private string name;
         public string Name
@@ -41,20 +44,51 @@ namespace Emp{
             Console.WriteLine("Name : "+name +" "+surName) ;
         }
    }
-   class Program
+   //Emp_3 is inherited from Employee
+   class Employee_3 : EmpLoyeeDetails
+    { 
+        private string name;
+        //To assign a value for private string name
+        public string Name
+        { 
+            get{
+                return name;
+            } 
+            set{
+                name = value;
+            }
+        }
+        //Diplay the name with surName
+        public void Display()
+        {
+            Console.WriteLine("Name : "+name +" "+surName) ;
+        }
+   }
+   public class Program
    {  
+      
        public static void Main(string[] args)  
         {  
-            //Object for Programmer1 class
-            Programmer1 p1 = new Programmer1();
-
-            //Object for Programmer2 class
-            Programmer2 p2 = new Programmer2();
-
+            //Object creation for Employee_1 class
+            Employee_1 p1 = new Employee_2();
+          
+            //Object creation for Employee_2 class
+            Employee_2 p2 = new Employee_2();
+            
+            //Object creation for Employee_3 class
+            Employee_3 p2 = new Employee_3();
+          
+            //setter method gets invoked in Employee_1 class 
             p1.Name= "Kavin";  
+            //setter method gets invoked in Employee_2 class
             p2.Name = "Abinav";
+            //setter method gets invoked in Employee_3 class
+            p3.Name = "Ram";
+            
+            //Caling Display method
             p1.Display(); 
             p2.Display();
+            p3.Display();
         }  
     }
 }
